4a49e7b4d286ef083e2bc35a846828ebcab12a88
laptopadventure <bazelart13@gmail.com>
2022-03-17 20:56:36 -0700

    error handling

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,7 @@ class Main extends React.Component {
     this.state = {
       searchInput: "",
       locationResult: null,
+      locError: '',
     }
   }
 
@@ -16,18 +17,40 @@ class Main extends React.Component {
 
   queryLocation = async event => {
     event.preventDefault()
-    if(!this.state.searchInput) {
-      return //no 400s!
-    }
+    // if(!this.state.searchInput) {
+    //   return //no 400s!
+    // }
     const infoUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchInput}&format=json`
     try {
       const infoRes = await axios.get(infoUrl);
       const target = infoRes.data[0]
       this.setState({
         locationResult: target,
+        locError: '',
       })
     } catch (error) {
-      console.warn(error)
+      let reason = ''
+      let readableError = error.toJSON()
+      switch(readableError.status) {
+        case 400: {
+          reason = 'Status 400. Please, enter a location!'
+          break
+        }
+        case 404: {
+          reason = 'Status 404. Your location couldn\'t be found, wow!'
+          break
+        }
+        case 500: {
+          reason = 'Status 500. The location grabber server is dead!'
+          break
+        }
+        default:
+          reason = 'Something on our end went wrong!'
+      }
+      this.setState({
+        locationResult: null,
+        locError: reason,
+      })
     }
   }
 
@@ -40,6 +63,13 @@ class Main extends React.Component {
             <p>{this.state.locationResult.display_name}</p>
             <p>{this.state.locationResult.lat}, {this.state.locationResult.lon}</p>
             <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchInput}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="Map!" fluid />
+          </div>
+        )}
+        {this.state.locError && (
+          <div className="loc-error">
+            <p>
+              {this.state.locError}
+            </p>
           </div>
         )}
       </div>
